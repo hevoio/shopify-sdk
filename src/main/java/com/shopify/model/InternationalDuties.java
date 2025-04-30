cd15d88d647ad917977d89c8aa052d9ac9346a44
Hevo-Tawish <tawish.singh@hevodata.com>
2025-04-30 17:10:17 +0530

    bugfix/HEVO-16181/add-fields-in-fulfillment-items

---
+++ src/main/java/com/shopify/model/InternationalDuties.java
@@ -0,0 +1,18 @@
+package com.shopify.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class InternationalDuties {
+    @XmlElement(name = "incoterm")
+    private String incoterm;
+}
--- src/main/java/com/shopify/model/ShopifyFulfillmentOrder.java
+++ src/main/java/com/shopify/model/ShopifyFulfillmentOrder.java
@@ -1,12 +1,15 @@
 package com.shopify.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.shopify.model.adapters.DateTimeAdapter;
 import lombok.Data;
+import org.joda.time.DateTime;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.util.List;
 
 @XmlRootElement
@@ -37,12 +40,22 @@ public class ShopifyFulfillmentOrder {
     private ShopifyDeliveryMethod deliveryMethod;
     @XmlElement(name = "fulfill_by")
     private String fulfillBy;
+    @XmlElement(name = "fulfill_at")
+    private String fulfillAt;
     @XmlElement(name = "fulfillment_holds")
     private List<FulfillmentHolds> fulfillmentHolds;
     @XmlElement(name = "include_financial_summaries")
     private boolean includeFinancialSummaries;
     @XmlElement(name = "include_order_reference_fields")
     private boolean includeOrderReferenceFields;
+    @XmlElement(name = "international_duties")
+    private InternationalDuties internationalDuties;
     @XmlElement(name = "presented_name")
     private String presentedName;
+    @XmlElement(name = "created_at")
+    @XmlJavaTypeAdapter(DateTimeAdapter.class)
+    private DateTime createdAt;
+    @XmlElement(name = "updated_at")
+    @XmlJavaTypeAdapter(DateTimeAdapter.class)
+    private DateTime updatedAt;
 }
